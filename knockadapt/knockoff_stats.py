2f089f190878dea3de03f6ac042cde6566018c6b
Asher Spector <amspector100@gmail.com>
2019-11-17 01:41:13 -0500

    Added pyglm fitting

--- knockadapt/knockoff_stats.py
+++ knockadapt/knockoff_stats.py
@@ -2,11 +2,19 @@ import warnings
 import numpy as np
 from sklearn import linear_model
 from group_lasso import GroupLasso
+from pyglmnet import GLM
 from statsmodels.stats.moment_helpers import cov2corr
 
-from .utilities import random_permutation
+from .utilities import random_permutation_inds
 
-DEFAULT_REG_VALS = np.logspace(-2, 1, base = 10, num = 5)
+DEFAULT_REG_VALS = np.logspace(-1, 2, base = 10, num = 5)
+
+
+def calc_mse(model, X, y):
+    """ Gets MSE: super simple """ 
+    preds = model.predict(X)
+    resids = (preds - y)/y.std()
+    return np.sum((resids)**2)
 
 def calc_LCD(Z, groups):
     """
@@ -62,15 +70,16 @@ def calc_LSM(Z, groups):
         
     return W_group
 
-def calc_nongroup_LSM(X, knockoffs, y, groups = None, **kwargs):
-    """ Calculates difference between average group Lasso signed maxs. 
-    Does NOT use a group lasso regression class, unfortunately.
+
+def calc_lambda_paths(X, knockoffs, y, groups = None, **kwargs):
+    """ Calculates locations at which X/knockoffs enter lasso 
+    model when regressed on y.
     :param X: n x p design matrix
     :param knockoffs: n x p knockoff matrix
     :param groups: p length numpy array of groups
-    :param kwargs: kwargs for sklearn Lasso class
-    """
-    
+    :param kwargs: kwargs for sklearn Lasso class 
+     """
+
     # Bind data
     n = X.shape[0]
     p = X.shape[1]
@@ -94,16 +103,54 @@ def calc_nongroup_LSM(X, knockoffs, y, groups = None, **kwargs):
         else:
             Z[i] = alphas[np.where(coefs[i] != 0)[0][0]]
 
+    return Z
+
+
+def calc_nongroup_LSM(X, knockoffs, y, groups = None, **kwargs):
+    """ Calculates difference between average group Lasso signed maxs. 
+    Does NOT use a group lasso regression class, unfortunately.
+    :param X: n x p design matrix
+    :param knockoffs: n x p knockoff matrix
+    :param groups: p length numpy array of groups
+    :param kwargs: kwargs for sklearn Lasso class
+    """
+        
+    # Z statistics
+    Z = calc_lambda_paths(X, knockoffs, y, groups, **kwargs)
+
     # Calculate group LSM
     W_group = calc_LSM(Z, groups)
         
     return W_group
 
-def fit_group_lasso(X, knockoffs, y, groups, **kwargs):
+
+
+def calc_nongroup_LCD(X, knockoffs, y, groups = None, **kwargs):
+    """ Calculates difference in absolute coefficient sums over groups. 
+    Uses the place where X/knockoffs enter the path, not the coefficients.
+    This and the prev function should be renamed.
+    :param X: n x p design matrix
+    :param knockoffs: n x p knockoff matrix
+    :param groups: p length numpy array of groups
+    :param kwargs: kwargs for sklearn Lasso class
+    """
+        
+    # Z statistics
+    Z = calc_lambda_paths(X, knockoffs, y, groups, **kwargs)
+
+    # Calculate group LSM
+    W_group = calc_LCD(Z, groups)
+        
+    return W_group
+
+def fit_group_lasso(X, knockoffs, y, groups, 
+                    use_pyglm = True, **kwargs):
     """ Fits a group lasso model.
     :param X: n x p design matrix
     :param knockoffs: n x p knockoff matrix
     :param groups: p length numpy array of groups
+    :param use_pyglm: If true, use the pyglmnet grouplasso
+    Else use the regular one
     :param kwargs: kwargs for group-lasso GroupLasso class.
     In particular includes reg_vals, a list of regularizations
     (lambda values) which defaults to [(0.05, 0.05)]. In each
@@ -121,20 +168,35 @@ def fit_group_lasso(X, knockoffs, y, groups, **kwargs):
     # By default, all variables are their own group
     if groups is None:
         groups = np.arange(0, p, 1)
-     
+        m = p
+    else:
+        m = np.unique(groups).shape[0]
+
     # Make sure variables and their knockoffs are in the same group
     # This is necessary for antisymmetry
     doubled_groups = np.concatenate([groups, groups], axis = 0)
 
-    # Standardize - dramatically reduces performance for some reason
-    # features = (features - features.mean())/features.std()
-    # y = (y - y.mean())/y.std()
+    # Randomize coordinates to make sure everything is symmetric
+    inds, rev_inds = random_permutation_inds(2*p)
+    features = features[:, inds]
+    doubled_groups = doubled_groups[inds]
+
+    # Standardize - important for pyglmnet performance, 
+    # highly detrimental for group_lasso performance
+    if use_pyglm:
+        features = (features - features.mean())/features.std()
+        y = (y - y.mean())/y.std()
+
+        # Weird behavior pyglmnet
+        doubled_groups = doubled_groups - 1
 
     # Get regularizations
     if 'reg_vals' in kwargs:
         reg_vals = kwargs['reg_vals']
+        kwargs.pop('reg_vals')
     else:
         reg_vals = [(x,x) for x in DEFAULT_REG_VALS]
+
     
     # Fit model
     best_gl = None
@@ -142,38 +204,52 @@ def fit_group_lasso(X, knockoffs, y, groups, **kwargs):
     for group_reg, l1_reg in reg_vals:
 
         # Fit
-        gl = GroupLasso(groups=doubled_groups, tol = 5e-2, 
-                        group_reg = group_reg, l1_reg = l1_reg,
-                        **kwargs)
-        gl.fit(features, y.reshape(n, 1))
+        if not use_pyglm:
+            gl = GroupLasso(groups=doubled_groups, tol = 5e-2, 
+                            group_reg = group_reg, l1_reg = l1_reg,
+                            **kwargs)
+            gl.fit(features, y.reshape(n, 1))
+            score = -1*calc_mse(gl, features, y.reshape(n, 1))
+        else:
+            gl = GLM(distr='gaussian',
+                     tol=5e-2, group=doubled_groups, alpha=1.0,
+                     learning_rate=3, max_iter=20,
+                     reg_lambda = l1_reg,
+                     solver = 'cdfast')
+            gl.fit(features, y)
+            score = -1*calc_mse(gl, features, y)
 
         # Score, possibly select
-        score = gl.score(features, y.reshape(n, 1))
         if score > best_score:
             best_score = score
             best_gl = gl
 
     warnings.simplefilter("always")
 
-    return best_gl
+    return best_gl, rev_inds
 
 
-def group_lasso_LSM(X, knockoffs, y, groups, **kwargs):
+def group_lasso_LSM(X, knockoffs, y, groups, use_pyglm = True, **kwargs):
     """ Calculates mean group Lasso signed max. 
     :param X: n x p design matrix
     :param knockoffs: n x p knockoff matrix
     :param groups: p length numpy array of groups
     :param kwargs: kwargs for group-lasso GroupLasso class
     """
-    gl = fit_group_lasso(X, knockoffs, y, groups = None, **kwargs)
+    gl, rev_inds = fit_group_lasso(X, knockoffs, y, groups = groups,
+                                   use_pyglm = use_pyglm, **kwargs)
 
     # Create LSM statistics
-    Z = gl.coef_
+    if use_pyglm:
+        Z = gl.beta_[rev_inds]
+    else:
+        Z = gl.coef_[rev_inds]
     W = calc_LSM(Z, groups)
     return W
 
 
-def group_lasso_LCD(X, knockoffs, y, groups = None, **kwargs):
+def group_lasso_LCD(X, knockoffs, y, groups = None,
+                    use_pyglm = True, **kwargs):
     """ Calculates group Lasso coefficient difference. 
     :param X: n x p design matrix
     :param knockoffs: n x p knockoff matrix
@@ -183,12 +259,15 @@ def group_lasso_LCD(X, knockoffs, y, groups = None, **kwargs):
     we set W(X, knockoffs, y) = 
     sum(abs coeff of X) - sum(abs coeff of knockoffs)
     """
-    gl = fit_group_lasso(X, knockoffs, y, groups = None, **kwargs)
+    gl, rev_inds = fit_group_lasso(X, knockoffs, y, groups = groups, 
+                                   use_pyglm = use_pyglm, **kwargs)
 
     # Create LCD statistics
-    Z = gl.coef_
+    if use_pyglm:
+        Z = gl.beta_[rev_inds]
+    else:
+        Z = gl.coef_[rev_inds]
     W = calc_LCD(Z, groups)
-
     return W
 
 def calc_data_dependent_threshhold(W, fdr=0.10, offset=1):
@@ -201,7 +280,7 @@ def calc_data_dependent_threshhold(W, fdr=0.10, offset=1):
     Else, if offset = 1, use knockoff+ (controls exact FDR).
     """
 
-    # Possibly values for Ts
+    # Possible values for Ts
     Ts = sorted(np.abs(W))
     Ts = np.concatenate([np.array((0,)), Ts], axis = 0)
     
@@ -210,7 +289,7 @@ def calc_data_dependent_threshhold(W, fdr=0.10, offset=1):
         return ((W <= -t).sum() + offset)/max(1, np.sum(W >= t))
     hat_fdp = np.vectorize(hat_fdp)
     ratios = hat_fdp(Ts)
-    
+
     # Find maximum
     acceptable = Ts[ratios <= fdr]
     if acceptable.shape[0] == 0:
